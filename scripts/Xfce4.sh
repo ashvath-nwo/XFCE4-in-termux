7fa17ab43ae3d8ab8c0cd00a23745e716a823511
ashvath-nwo <mashaultrafunkthecoder@gmail.com>
2025-04-19 22:37:29 +0530

    Update Xfce4.sh

--- scripts/Xfce4.sh
+++ scripts/Xfce4.sh
@@ -4,6 +4,8 @@ pkg install termux-x11-nightly -y
 pkg install xfce4 -y
 pkg install xfce4-session -y
 pkg install dbus-x11 -y
+curl -o setup.sh https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/installpulseaudio.sh && chmod +rwx setup.sh && ./setup.sh && rm setup.sh
 echo 'termux-x11 :1 -xstartup "dbus-launch --exit-with-session xfce4-session"' > $PREFIX/bin/start-termux-x11 && chmod +x $PREFIX/bin/start-xfce4
 echo XFCE4 For Termux is installed. Type the command start-xfce4 to start XFCE4.
 echo This method uses Termux:x11 and not VNC.
+echo Thanks to Nguyen Bao An Bui for the PulseAudio Script
